4462db36c97d1788f20c8f9b54cbe8ef5a203ca9
srikarmareddy <rikars.mareddy@gmail.com>
2022-10-08 16:42:51 -0400

    adding changes to js

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -35,11 +35,11 @@ async function mainEvent() { // the async keyword means we can make API requests
       // It does not include any of your form values, though
     */
 
-    const results = await fetch('/api/foodServicePG');
+    // const results = await fetch('/api/foodServicePG');
+    const fetchQuery = new URLSearchParams(formProps)
+    const results = await fetch(`/api/foodServicePG?${fetchQuery}`);
     /*
    ## Get request with query parameters
-
-      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
 
       The above request uses "string interpolation" to include a encoded version of your form values
       It works because it has a ? in the string

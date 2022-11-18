596655151a18671728dc6db5bcd8e55d5060f9d1
srikarmareddy <rikars.mareddy@gmail.com>
2022-11-18 12:11:41 -0500

    fixed linting

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -12,9 +12,9 @@
 */
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
 }
 function injectHTML(list) {
   console.log('fired injectHTML');
